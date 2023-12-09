0812bef9a33520f3916b6511cf57c5e9fed6d1ac
Jessica Ho <jqianh1888@icloud.com>
2023-12-08 20:06:11 -0800

    just add token and it also works now.

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,25 +1,48 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const options = {
         method: 'GET',
-        url: 'https://jsearch.p.rapidapi.com/search',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-          'X-RapidAPI-Key': '',
+          'X-RapidAPI-Key': "",
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }, 
-        params: {
-            query: 'Python developer in Texas, USA',
-            page: '1',
-            num_pages: '1'
-        },
+        params: { ...query },
     };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+
+            setData(response.data.data);
+            setIsLoading(false);
+        }   catch (error) {
+            setError(error);
+            alert('There is an error')   
+        }   finally {
+            setIsLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
 }
 
+export default useFetch ;
 
 
